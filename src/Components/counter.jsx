397a3ce12e7979dbc99a8475f1c0cb73e3901a81
infinityshades <tash.aneiq@gmail.com>
2020-02-13 07:42:09 +0100

    Binding event handlers

--- src/Components/counter.jsx
+++ src/Components/counter.jsx
@@ -3,25 +3,34 @@ import React, { Component } from 'react'
 class Counter extends Component {
     state = {
         count: 0,
-        tags: ['tag1', 'tag2', 'tag3']
+        // tags: ['tag1', 'tag2', 'tag3']
     };
 
+    //To bind eventhandlers
+    // constructor(){
+    //     super()
+    //     this.handleIncrement = this.handleIncrement.bind(this)
+    // }
     // conditional rendering
     renderTags() {
         if (this.state.tags.length === 0) return <p>There are no tags!</p>
         return <ul> {this.state.tags.map(tag => <li key={tag}>{tag}</li>)} </ul>;
     }
 
+    //Use arrow functions don't rebind this keyword, it inherits it
+    handleIncrement = ()=> {
+        console.log('increment clicked', this);
+    }
     render() {
 
         return (
             <>
                 <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
-                <button className="btn btn-secondary btn-sm">Increment</button>
+                <button onClick ={this.handleIncrement} className="btn btn-secondary btn-sm">Increment</button>
                 
                 {/* conditonal rendering */}
-                {this.state.tags.length === 0 && 'Please create a list'}
-                {this.renderTags()}
+                {/* {this.state.tags.length === 0 && 'Please create a list'}
+                {this.renderTags()} */}
             </>
         );
     }
