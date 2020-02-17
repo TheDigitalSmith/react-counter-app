49b46192f82519f8988387408eed6ed5c4d9f660
infinityshades <tash.aneiq@gmail.com>
2020-02-17 18:00:29 +0100

    object destructuring args

--- src/Components/counters.jsx
+++ src/Components/counters.jsx
@@ -5,14 +5,15 @@ class Counters extends Component {  //Shorcut key for creating class component =
     
     render() { 
         console.log(this.props)
+        const {onReset, onIncrement, onDelete, counters} = this.props
         return (
             <div>
-                <button onClick ={this.props.onReset} className="btn btn-primary btn-sm m-2">RESET</button>
-                {this.props.counters.map(c => <Counter 
+                <button onClick ={onReset} className="btn btn-primary btn-sm m-2">RESET</button>
+                {counters.map(c => <Counter 
                 key= { c.id} 
                 counter = {c}
-                onDelete ={this.props.onDelete}
-                onIncrement = {this.props.onIncrement}
+                onDelete ={onDelete}
+                onIncrement = {onIncrement}
                 />)}
                 {/* the value is passed down into the child component as a prop */}
             </div>
--- src/Components/navbar.jsx
+++ src/Components/navbar.jsx
@@ -1,11 +1,11 @@
 import React, { Component } from 'react';
 
 //Stateless functional component, shortcut = sfc
-const Navbar = (props) =>{
+const Navbar = ({totalCounters}) =>{
     return ( 
         <nav className = "navbar navbar-light bg-light">
             <a className = "navbar-brand" href="#">
-                Navbar <span className="badge badge-pill badge-secondary">{props.totalCounters}</span>
+                Navbar <span className="badge badge-pill badge-secondary">{totalCounters}</span>
             </a>
         </nav>
      );
